2961c43c97904cb235d93a75a2a816c6800b215f
Risha Mars <mars@buoyant.io>
2016-12-15 18:02:20 -0800

    Add readme entry

--- admin/src/main/resources/io/buoyant/admin/spec/QuerySpec.js
+++ admin/src/main/resources/io/buoyant/admin/spec/QuerySpec.js
@@ -1,17 +1,32 @@
 describe("Query", function() {
   var Query = require('../js/query.js');
 
-  describe("Query", function() {
-
-    it("should fail on a failure", function() {
-      expect(1 + 1).toBe(2);
-    });
-
+  describe("generalQuery", function() {
     it("builds a query for a router and metric", function() {
       var routerName = "fooRouter";
       var query = Query.clientQuery().withRouter(routerName).withMetric("fooMetric").build();
       var expectedRegex = new RegExp('^rt\/(fooRouter)\/dst\/id\/(.*)\/(fooMetric)$');
       expect(query).toEqual(expectedRegex);
-    })
+    });
+  });
+
+  describe("clientQuery", function() {
+    it("should fail on a failure", function() {
+      expect(1 + 1).toBe(2);
+    });
+
+    it("builds a query for a router and metric and all client", function() {
+      var routerName = "fooRouter";
+      var query = Query.clientQuery().withRouter(routerName).withMetric("fooMetric").allClients().build();
+      var expectedRegex = new RegExp('^rt\/(fooRouter)\/dst\/id\/(.*)\/(fooMetric)$');
+      expect(query).toEqual(expectedRegex);
+    });
+
+    it("builds a query for a router and metric and a client", function() {
+      var routerName = "fooRouter";
+      var query = Query.clientQuery().withRouter(routerName).withMetric("fooMetric").withClient("fooClient").build();
+      var expectedRegex = new RegExp('/^rt\/(fooRouter)\/dst\/id\/(fooClient)\/(fooMetric)$');
+      // expect(query).toEqual(expectedRegex);
+    });
   });
 });
